ff27cbb0092c70af56277358544fcc25736ae743
ds1bkf <ds1bkf@gmail.com>
2026-07-08 11:25:31 +0900

    카운터 네임스페이스 정식 초기화(live)

--- tracker.js
+++ tracker.js
@@ -1,7 +1,7 @@
 /* steel-briefing visit tracker (fire-and-forget, no PII, no IP) */
 (function () {
   try {
-    var NS = "steelbrief-ds1bkf-v1";
+    var NS = "steelbrief-ds1bkf-live";
     var BASE = "https://abacus.jasoncameron.dev/hit/" + NS + "/";
     // KST(UTC+9) 기준 오늘 날짜 키
     var now = new Date();
@@ -13,4 +13,4 @@
     fetch(BASE + "total", { keepalive: true }).catch(function () {});
     fetch(BASE + "d-" + day, { keepalive: true }).catch(function () {});
   } catch (e) {}
-})();
+})()
